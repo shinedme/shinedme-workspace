b07800a77b5810ef2fc3e5d5f08ea9942e7399a6
Bo Yao <bo@nearprotocol.com>
2020-08-09 00:34:46 -0700

    docker-compose fully works

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Aggregate workspace for work with shinedme blockchain node, frontend and helper services
 
 ## Run all services with docker-compose
+
+### Prerequisite:
+- git, GNU make
+- docker-compose 1.25.0 and above
+
 ```
 git submodule --init --recursive
 make
--- start_ipfs.sh
+++ start_ipfs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [ ! -f /data/ipfs/config ]; then
-    ipfs init
-    ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin  '["*"]'
-    ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST", "GET"]'
-fi
-ipfs daemon --migrate=true
+pwd
+ls
+sed "\$i $(cat cors.sh)" /usr/local/bin/start_ipfs > start_ipfs2.sh
+cat start_ipfs2.sh
+chmod +x ./start_ipfs2.sh
+/sbin/tini -- ./start_ipfs2.sh daemon --migrate=true
